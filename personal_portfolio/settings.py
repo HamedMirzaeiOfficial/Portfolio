6a0713ed9f3c59a3dd240118600c7566c1057251
Hamed Mirzaei <HamedMirzaei2001Official@gmail.com>
2024-08-16 11:30:37 +0330

    update

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -127,10 +127,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -33,6 +33,6 @@ urlpatterns = [
 
 
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+# Development
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
