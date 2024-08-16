576bf5447000aeb7a49f656447137a081b57ec96
Hamed Mirzaei <HamedMirzaei2001Official@gmail.com>
2024-08-16 11:33:28 +0330

    update

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -127,10 +127,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "core/static_files"),
+]
 
 
 MEDIA_URL = '/media/'
